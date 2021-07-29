4245d810979ca1b065c9de637513275e243c3284
Joonatan@work <joonatanrenel@gmail.com>
2021-07-29 10:25:18 +0300

    Initial commit

---
+++ CalculatorTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace CalculatorTest
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool TryEvaluate(string expr, out BigInteger value, out string error)
+        {
+            /* Placeholder. */
+            value = BigInteger.Zero;
+            error = "None";
+            return true;
+        }
+    }
+}
